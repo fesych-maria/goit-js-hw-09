64172f339cf466f6f391a712828ff3cbcc15eb41
maria <fesychmarimia@gmail.com>
2025-02-02 17:31:38 +0100

    second task

---
+++ src/js/2-form.js
@@ -0,0 +1,57 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+refs.form.addEventListener('input', saveData);
+
+function saveData(e) {
+  formData.email = e.currentTarget.querySelector('[name="email"]').value;
+  formData.message = e.currentTarget.querySelector('[name="message"]').value;
+  saveToLs(STORAGE_KEY, formData);
+}
+
+function initPage() {
+  const dataFromLs = getFromLs(STORAGE_KEY);
+  formData.email = dataFromLs?.email || '';
+  formData.message = dataFromLs?.message || '';
+  refs.form.querySelector('[name="email"]').value = formData.email;
+  refs.form.querySelector('[name="message"]').value = formData.message;
+}
+initPage();
+
+refs.form.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+  e.preventDefault();
+  if (formData.email.trim() === '' || formData.message.trim() === '') {
+    alert('Fill please all fields');
+  } else {
+    formData.message = formData.message.trim();
+    console.log(formData);
+    localStorage.removeItem(STORAGE_KEY);
+    formData.email = '';
+    formData.message = '';
+    refs.form.reset();
+  }
+}
+
+function saveToLs(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getFromLs(value) {
+  const data = localStorage.getItem(value);
+  try {
+    const parsedData = JSON.parse(data);
+    return parsedData;
+  } catch {
+    return data;
+  }
+}
+
+refs.form.addEventListener('');
